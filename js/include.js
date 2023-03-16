6ce781561a53a3f9f607f0c97ec7fa0042e4a780
ramcam <guilleramirezc@gmail.com>
2023-03-16 20:07:15 +0100

    Rewrote import html function using .each, moved js to independent files and reorganized file structure

--- js/include.js
+++ js/include.js
@@ -1,3 +1,17 @@
-$(function(){
-    $("#includedContent").load("content/algebra_groups/01_groups_intro.html"); 
-  });
+loadHtml = function(filepath, filenames){
+  /*var filepath = "content/"
+  var filenames = [
+      "01_groups_intro",
+      "02_groups_intro2",
+      "03_new_post"
+      ]*/
+  var html = ""
+  for(let i=0; i<filenames.length; i++){
+      html += "<div class='external-content' id="+filenames[i]+"></div>"
+  }
+  $(".math-content").html( html );
+  $(".external-content").each(
+    function(index){
+      $(this).load(filepath + filenames[index] + ".html",
+      function(){MathJax.typesetPromise();});});
+  }
