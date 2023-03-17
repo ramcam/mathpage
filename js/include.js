268233f433955f469bdfa6918fbbcdb42183c44f
ramcam <guilleramirezc@gmail.com>
2023-03-17 18:59:11 +0100

    Fixed mathjax not typesetting inline math when moved to external .js file. Removed mathjax config file.

--- js/include.js
+++ js/include.js
@@ -1,10 +1,4 @@
 loadHtml = function(filepath, filenames){
-  /*var filepath = "content/"
-  var filenames = [
-      "01_groups_intro",
-      "02_groups_intro2",
-      "03_new_post"
-      ]*/
   var html = ""
   for(let i=0; i<filenames.length; i++){
       html += "<div class='external-content' id="+filenames[i]+"></div>"
