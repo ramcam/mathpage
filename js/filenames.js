d47b7b1bed2ac133e1c4eb324a3b3e0b4c215ea6
ramcam <guilleramirezc@gmail.com>
2023-04-02 21:02:04 +0200

    Added accordion navigation and placeholder links for different chapters

--- js/filenames.js
+++ js/filenames.js
@@ -1,6 +1,24 @@
-var filenames = [
+const filenames = [
     "01_quintic_intro",
     "01_groups_intro",
     "02_exploring_groups",
     "03_new_post"
     ];
+
+const content = {
+    "Algebra":{
+        "Groups":[
+            "01_groups_intro",
+            "02_exploring_groups"
+        ],
+        "Rings":[],
+        "Galois Theory":[
+            "01_quintic_intro"
+        ]
+    },
+    "Analysis":{
+        "Metric spaces":[],
+        "Compactness":[],
+        "Operators":[]
+    }
+};
--- js/layout.js
+++ js/layout.js
@@ -14,3 +14,40 @@ makeFooter = function(filepath="html/"){
         function(){MathJax.typesetPromise();});
 
 }
+
+toggleShow =  function(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+makeNavContent = function(filepath="html/"){
+    // first create and append container
+    var div = document.createElement("div");
+    div.setAttribute("id", "nav-content");
+    div.setAttribute("class", "w3-container w3-hide");
+    jQuery.each(content, function(i, val){
+        // create sections
+        var sectionDiv = document.createElement("div");
+        sectionDiv.setAttribute("class", "w3-container w3-cell section");
+        sectionDiv.innerHTML = i;
+        jQuery.each(val, function(j, val2){
+            // create chapters
+            var chapter = document.createElement("div");
+            chapter.setAttribute("class", "chapter");
+            chapter.innerHTML = "<a href=''>"+j+"</a>";
+            sectionDiv.appendChild(chapter);
+        });
+        div.appendChild(sectionDiv);
+        
+    });
+    document.body.appendChild(div);
+    // now add divs to 
+    // jQuery.each(content, function(i, val){})
+    //$("#nav-content").load(filepath + "navContent.html",
+    //    function(){MathJax.typesetPromise();});
+
+}
